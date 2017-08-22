b904aa9a75e67fdb94792d9a78cde578b133eece
Daniel Morales <danielmoralesx@hotmail.com>
2017-08-22 16:36:15 -0300

    Add new test

--- inst/tests/test-dmorales.fars.R
+++ inst/tests/test-dmorales.fars.R
@@ -1,4 +1,5 @@
 test_that("make_filename", {
   expect_identical(make_filename("2013"), "accident_2013.csv.bz2")
   expect_identical(make_filename("4321"), "accident_4321.csv.bz2")
+  expect_identical(make_filename("89"), "accident_89.csv.bz2")
 })
